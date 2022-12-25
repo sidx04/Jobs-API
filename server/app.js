9f3ec45aaf82bf12f8e1e93033d60ab31728522c
Onion-Man <siddpal04@gmail.com>
2022-12-26 01:59:16 +0530

    db config

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app=express()
 
 
 // connectDB
-
+const connectDB=require('./db/connect')
 
 // routers
 const authRouter=require('./routes/auth')
@@ -15,10 +15,10 @@ const jobsRouter=require('./routes/jobs')
 // error handler
 const notFoundMiddleware=require('./middleware/not-found')
 const errorHandlerMiddleware=require('./middleware/error-handler')
-app.use(express.json())
 
 
 // routes
+app.use(express.json())
 app.use('/api/v1/auth',authRouter)  
 app.use('/api/v1/jobs',jobsRouter)
 
@@ -29,10 +29,12 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
-  } catch (error) {
+  }
+  catch (error) {
     console.log(error);
   }
 };
