7914ebdfc5096eba05ab1a30a0c472668d47a0a9
Onion-Man <siddpal04@gmail.com>
2022-12-28 23:58:09 +0530

    creating job fix

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -4,7 +4,7 @@ const {BadRequestError,NotFoundError}=require('../errors')
 
 const getAllJobs=async(req,res)=>{
     const jobs=await Job.find({createdBy:req.user.userId}).sort('createdAt')
-    res.status(StatusCodes.OK).json({jobs, count:jobs.length})
+    res.status(StatusCodes.OK).json({count:jobs.length, jobs})
 }
 
 const getJob=async(req,res)=>{
