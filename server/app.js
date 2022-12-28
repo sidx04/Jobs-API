a7181aecde34ca20953e058b71bb17d3b3e9b68b
Onion-Man <siddpal04@gmail.com>
2022-12-29 04:57:01 +0530

    security packaging

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 require('express-async-errors')
 const express=require('express')
+const helmet=require('helmet')
+const cors=require('cors')
+const xss=require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
 const app=express()
 
 
@@ -18,9 +23,14 @@ const errorHandlerMiddleware=require('./middleware/error-handler')
 
 
 // routes
+app.use(rateLimiter)
 app.use(express.json())
 app.use('/api/v1/auth',authRouter)  
 app.use('/api/v1/jobs',authenticateUser, jobsRouter)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
