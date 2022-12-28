9bc885f07874e6fda3c0940f915617585dadc354
Onion-Man <siddpal04@gmail.com>
2022-12-29 01:05:04 +0530

    get job

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -2,25 +2,40 @@ const Job=require('../models/Job')
 const {StatusCodes}=require('http-status-codes')
 const {BadRequestError,NotFoundError}=require('../errors')
 
+// all job listings for the user
 const getAllJobs=async(req,res)=>{
     const jobs=await Job.find({createdBy:req.user.userId}).sort('createdAt')
     res.status(StatusCodes.OK).json({count:jobs.length, jobs})
 }
 
+// single job
 const getJob=async(req,res)=>{
-    res.send('get job')
+    const {user:{userId}, params:{id:jobId}}=req
+
+    const job=await Job.findOne({
+        _id:jobId,
+        createdBy:userId
+    })
+    if(!job){
+        throw new NotFoundError(`no job with ID: ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
+// add new job to the listing
 const createJob=async(req,res)=>{
     req.body.createdBy=req.user.userId
     const job=await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({job})
 }
 
+//  update job
 const updateJob=async(req,res)=>{
     res.send('update job')
 }
 
+// delete a job
 const deleteJob=async(req,res)=>{
     res.send('delete job')
 }
