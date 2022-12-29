3176d2cc4c5ca33008ff02f6eaf51af11f2d957f
Onion-Man <siddpal04@gmail.com>
2022-12-29 12:06:25 +0530

    security packaging done

--- server/app.js
+++ server/app.js
@@ -23,14 +23,20 @@ const errorHandlerMiddleware=require('./middleware/error-handler')
 
 
 // routes
-app.use(rateLimiter)
+app.set('trust proxy',1 )
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json())
-app.use('/api/v1/auth',authRouter)  
-app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.use('/api/v1/auth',authRouter)  
+app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
